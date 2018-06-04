c76d7eca86402697b243fcf01869a877bb2865a7
Thomas Pollet <thomas.pollet@gmail.com>
2018-06-04 15:22:18 +0200

    update

---
+++ src/action/ActionList.js
@@ -0,0 +1,13 @@
+import CreateAction from "../components/actions/CreateAction";
+import EditAction from "../components/actions/EditAction";
+import DeleteAction from "../components/actions/DeleteAction";
+import AnalyzeAction from "../components/actions/AnalyzeAction";
+
+var actions = {
+	'CreateAction' : CreateAction,
+	'EditAction' : EditAction,
+	'DeleteAction' : DeleteAction,
+	'AnalyzeAction' : AnalyzeAction
+}
+
+export default actions
