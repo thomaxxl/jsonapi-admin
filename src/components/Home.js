3a368d1aea54199d8b739c074ec4e00eaeae3044
Thomas Pollet <thomas.pollet@gmail.com>
2018-06-24 10:16:33 +0200

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,30 @@
 import React, { PropTypes } from 'react';
-
 import * as Param from '../Config'
+import APP from '../Config.json';
 
 const Home = () => {
+
+    console.log('hom',Param)
     return (
         <div>
             <header>
                 <div className="jumbotron jumbotron-fluid bg-info text-white text-center">
                     <div className="container">
                         <h1 className="display-4">jsonapi-admin UI</h1>
-                        <div>react+redux js frontend for a jsonapi backend
-                            <br/>
-                            <a href="https://github.com/thomaxxl/jsonapi-admin">Github Source</a>
-                        </div>
-
+                        React+redux js frontend for a jsonapi backend
                     </div>
                 </div>
             </header>
             <div className="container">
- 
+                <ul>
+                    <li>
+                        <a href="https://github.com/thomaxxl/jsonapi-admin">Github</a>
+                    </li>
+                    <li>This framework implements CRUD operations on the jsonapi at {Param.URL} </li>
+                    <li>UI Configuration:
+                        <pre>{JSON.stringify(APP,null,2)}</pre>
+                    </li>
+                </ul>
             </div>
         </div>
     );
