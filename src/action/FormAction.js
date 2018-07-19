9968374490f1350a325815af38d21eadcb5852ce
Thomas Pollet <thomas.pollet@gmail.com>
2018-07-19 11:04:18 +0200

    up

---
+++ src/action/FormAction.js
@@ -0,0 +1,13 @@
+import * as ActionType from './ActionType';
+
+export const getFormResponse = form => ({
+    type: ActionType.GET_FORM_RESPONSE,
+    form
+});
+
+export function getFormAction(form) {
+    
+    return (dispatch) => {
+            dispatch(getFormResponse(form));
+    };
+}
